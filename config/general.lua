6969648c377683d6a196a58f868a73a083c9b02a
Am-I-Human <marisio.andrea@yahoo.it>
2025-08-13 09:56:02 +0200

    adding kitty ops

--- config/general.lua
+++ config/general.lua
@@ -1,6 +1,6 @@
 return {
-   term = 'wezterm',
-
+   term = 'xterm-kitty',
+   enable_kitty_graphics = true,
    -- behaviours
    automatically_reload_config = true,
    exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
