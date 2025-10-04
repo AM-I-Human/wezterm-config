9fd345b962737c77dc7d59bfb078bb574d2ad769
Am-I-Human <marisio.andrea@yahoo.it>
2025-10-04 16:08:53 +0200

    removing reload

--- config/general.lua
+++ config/general.lua
@@ -5,7 +5,7 @@ return {
    },
    default_domain = 'local',
    -- behaviours
-   automatically_reload_config = true,
+   automatically_reload_config = false,
    exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
    exit_behavior_messaging = 'Verbose',
    status_update_interval = 1000,
